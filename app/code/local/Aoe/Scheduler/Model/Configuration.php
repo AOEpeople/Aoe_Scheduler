d30060af746816779a0e147f41de234b10a0bc88
Fabrizio Branca <github@fabrizio-branca.de>
2011-09-09 13:07:21 +0200

    Added extra checks to avoid notices that might break the module output

--- app/code/local/Aoe/Scheduler/Model/Configuration.php
+++ app/code/local/Aoe/Scheduler/Model/Configuration.php
@@ -50,32 +50,29 @@ class Aoe_Scheduler_Model_Configuration extends Mage_Core_Model_Abstract {
 
 		$global = $this->getGlobalCrontabJobXmlConfig();
 		$cronExpr = null;
-		if ($global->schedule->config_path) {
+		if ($global && $global->schedule && $global->schedule->config_path) {
 			$cronExpr = Mage::getStoreConfig((string)$global->schedule->config_path);
 		}
-		if (empty($cronExpr) && $global->schedule->cron_expr) {
+		if (empty($cronExpr) && $global && $global->schedule && $global->schedule->cron_expr) {
 			$cronExpr = (string)$global->schedule->cron_expr;
 		}
 		if ($cronExpr) {
 			$this->setCronExpr($cronExpr);
 		}
-		$model = (string)$global->run->model;
-		if ($model) {
-			$this->setModel($model);
+		if ($global && $global->run && $global->run->model) {
+			$this->setModel((string)$global->run->model);
 		}
 
 		$configurable = $this->getConfigurableCrontabJobXmlConfig();
 		if ($configurable) {
 			if (is_object($configurable->schedule)) {
-				$cronExpr = (string)$configurable->schedule->cron_expr;
-				if ($cronExpr) {
-					$this->setCronExpr($cronExpr);
+				if ($configurable && $configurable->schedule && $configurable->schedule->cron_expr) {
+					$this->setCronExpr((string)$configurable->schedule->cron_expr);
 				}
 			}
 			if (is_object($configurable->run)) {
-				$model = (string)$configurable->run->model;
-				if ($model) {
-					$this->setModel($model);
+				if ($configurable && $configurable->run && $configurable->run->model) {
+					$this->setModel((string)$configurable->run->model);
 				}
 			}
 		}
