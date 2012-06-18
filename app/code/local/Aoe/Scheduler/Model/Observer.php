64cfe4af89c7c2e4b5cb24a29eb8f48ce85450d6
Tymoteusz Motylewski <t.motylewski@gmail.com>
2012-06-18 10:26:33 +0200

    Add support for black and white list of jobs to run
    This enables you to run multiple scheduler jobs in parallel, and have e.g designated cron job for some scheduler job.

--- app/code/local/Aoe/Scheduler/Model/Observer.php
+++ app/code/local/Aoe/Scheduler/Model/Observer.php
@@ -185,4 +185,40 @@ class Aoe_Scheduler_Model_Observer extends Mage_Cron_Model_Observer {
 		return $result;
 	}
 
+	public function getWhitelist() {
+		$whitelist = array();
+		if(getenv("SCHEDULER_WHITELIST") !== FALSE ) {
+			$whitelist = explode(',',getenv("SCHEDULER_WHITELIST"));
+		}
+		return $whitelist;
+	}
+
+	public function getBlacklist() {
+		$blacklist = array();
+		if(getenv("SCHEDULER_BLACKLIST") !== FALSE) {
+			$blacklist = explode(',',getenv("SCHEDULER_BLACKLIST"));
+		}
+		return $blacklist;
+	}
+
+	public function getPendingSchedules()
+	{
+		if (!$this->_pendingSchedules) {
+			$this->_pendingSchedules = Mage::getModel('cron/schedule')->getCollection()
+				->addFieldToFilter('status', Mage_Cron_Model_Schedule::STATUS_PENDING);
+
+			$whitelist = $this->getWhitelist();
+			if(!empty($whitelist)) {
+				$this->_pendingSchedules->addFieldToFilter('job_code', array('in'=> $whitelist));
+			}
+
+			$blacklist = $this->getBlacklist();
+			if(!empty($blacklist)) {
+				$this->_pendingSchedules->addFieldToFilter('job_code', array('nin'=> $blacklist));
+			}
+
+			$this->_pendingSchedules = $this->_pendingSchedules->load();
+		}
+		return $this->_pendingSchedules;
+	}
 }
