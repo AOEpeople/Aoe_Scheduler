f51dccc4209f31658f6f833ef1d4df40ca9f2fa2
Fabrizio Branca <fabrizio.branca@aoemedia.de>
2013-03-03 23:44:25 -0800

    Added heartbeat health message to the backend

--- app/code/community/Aoe/Scheduler/Helper/Data.php
+++ app/code/community/Aoe/Scheduler/Helper/Data.php
@@ -8,15 +8,14 @@
 class Aoe_Scheduler_Helper_Data extends Mage_Core_Helper_Abstract {
 
 
-
 	/**
 	 * Explodes a string and trims all values for whitespace in the ends.
 	 * If $onlyNonEmptyValues is set, then all blank ('') values are removed.
 	 *
 	 * @see t3lib_div::trimExplode() in TYPO3
-	 * @param string Delimiter string to explode with
-	 * @param string The string to explode
-	 * @param boolean If set, all empty values will be removed in output
+	 * @param $delim
+	 * @param string $string
+	 * @param bool $removeEmptyValues If set, all empty values will be removed in output
 	 * @return array Exploded values
 	 */
 	public function trimExplode($delim, $string, $removeEmptyValues=false) {
@@ -42,6 +41,7 @@ class Aoe_Scheduler_Helper_Data extends Mage_Core_Helper_Abstract {
 	/**
 	 * Decorate status values
 	 *
+	 * @param $status
 	 * @return string
 	 */
 	public function decorateStatus($status) {
@@ -86,7 +86,7 @@ class Aoe_Scheduler_Helper_Data extends Mage_Core_Helper_Abstract {
 	/**
 	 * Decorate time values
 	 *
-	 * @param string value
+	 * @param string $value
 	 * @param bool $echoToday if true "Today" will be added
 	 * @param string $dateFormat make sure Y-m-d is in it, if you want to have it replaced
 	 * @return string
@@ -104,6 +104,58 @@ class Aoe_Scheduler_Helper_Data extends Mage_Core_Helper_Abstract {
 			$value = str_replace(array_keys($replace), array_values($replace), $value);
 		}
 		return $value;
+	}
+
+
+	/**
+	 * Get last heartbeat
+	 */
+	public function getLastHeartbeat() {
+		if ($this->isDisabled('aoescheduler_heartbeat')) {
+			return false;
+		}
+		$schedules = Mage::getModel('cron/schedule')->getCollection(); /* @var $schedules Mage_Cron_Model_Mysql4_Schedule_Collection */
+		$schedules->getSelect()->limit(1)->order('executed_at DESC');
+		$schedules->addFieldToFilter('status', Mage_Cron_Model_Schedule::STATUS_SUCCESS);
+		$schedules->addFieldToFilter('job_code', 'aoescheduler_heartbeat');
+		$schedules->load();
+		if (count($schedules) == 0) {
+			return false;
+		}
+		$executedAt = $schedules->getFirstItem()->getExecutedAt();
+		$value = Mage::getModel('core/date')->date(NULL, $executedAt);
+		return $value;
+	}
+
+
+	/**
+	 * Diff between to times;
+	 *
+	 * @param $time1
+	 * @param $time2
+	 * @return int
+	 */
+	public function dateDiff($time1, $time2=NULL) {
+		if (is_null($time2)) {
+			$time2 = Mage::getModel('core/date')->date();
+		}
+		$time1 = strtotime($time1);
+		$time2 = strtotime($time2);
+		return $time2 - $time1;
+	}
+
+
+
+	/**
+	 * Check if job code is disabled in configuration
+	 *
+	 * @param $jobCode
+	 * @return bool
+	 */
+	public function isDisabled($jobCode) {
+		$disabledJobs = Mage::getStoreConfig('system/cron/disabled_crons');
+		$disabledJobs = $this->trimExplode(',', $disabledJobs);
+		return in_array($jobCode, $disabledJobs);
 	}
 
 }
--- app/code/community/Aoe/Scheduler/controllers/Adminhtml/AbstractController.php
+++ app/code/community/Aoe/Scheduler/controllers/Adminhtml/AbstractController.php
@@ -13,9 +13,40 @@ abstract class Aoe_Scheduler_Adminhtml_AbstractController extends Mage_Adminhtml
 	 * @return void
 	 */
 	public function indexAction() {
+		$this->checkHeartbeat();
+
 		$this->loadLayout();
+
 		$this->_setActiveMenu('system');
 		$this->renderLayout();
+	}
+
+
+	
+	/**
+	 * Check heartbeat
+	 */
+	protected function checkHeartbeat() {
+		if (!Mage::helper('aoe_scheduler')->isDisabled('aoescheduler_heartbeat')) {
+			$lastHeartbeat = Mage::helper('aoe_scheduler')->getLastHeartbeat();
+			if ($lastHeartbeat === false) {
+				// no heartbeat task found
+				$this->_getSession()->addError('No heartbeat task found. Check if cron is configured correctly.');
+			} else {
+				$timespan = Mage::helper('aoe_scheduler')->dateDiff($lastHeartbeat);
+				if ($timespan <= 5 * 60) {
+					$this->_getSession()->addSuccess('Scheduler is working');
+				}
+				if ($timespan > 5 * 60 && $timespan <= 60 * 60 ) {
+					// heartbeat wasn't executed in the last 5 minutes. Heartbeat schedule could be modified to not run every five minutes!
+					$this->_getSession()->addNotice(sprintf('Last heartbeat is older than %s minutes.', round($timespan/60)));
+				} else {
+					// everything ok
+					$this->_getSession()->addError('Last heartbeat is older than one hour. Please check your settings and your configuration!');
+				}
+			}
+
+		}
 	}
 
 
