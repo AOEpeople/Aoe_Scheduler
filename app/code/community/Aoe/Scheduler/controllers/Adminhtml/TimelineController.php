dbd2cf3e2ac54c5d246fb80faae69c306c2cff07
Andrey Legayev <andrey@ven.com>
2013-09-29 11:49:44 +0300

    Add ACL checks to adminhtml controllers

--- app/code/community/Aoe/Scheduler/controllers/Adminhtml/TimelineController.php
+++ app/code/community/Aoe/Scheduler/controllers/Adminhtml/TimelineController.php
@@ -9,4 +9,14 @@ require_once Mage::getModuleDir('controllers', 'Aoe_Scheduler').'/Adminhtml/Abst
  */
 class Aoe_Scheduler_Adminhtml_TimelineController extends Aoe_Scheduler_Adminhtml_AbstractController {
 
+
+    /**
+     * Acl checking
+     *
+     * @return bool
+     */
+    protected function _isAllowed() {
+        return Mage::getSingleton('admin/session')->isAllowed('system/aoe_scheduler/aoe_scheduler_timeline');
+    }
+
 }
