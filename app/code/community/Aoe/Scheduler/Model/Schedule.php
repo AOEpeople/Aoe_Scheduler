51931bbf5cbfce306bde8ced7b9fd34db451f551
Fabrizio Branca <fabrizio.branca@aoemedia.de>
2013-11-07 22:00:55 -0800

    Log exception on errors while isAlways check

--- app/code/community/Aoe/Scheduler/Model/Schedule.php
+++ app/code/community/Aoe/Scheduler/Model/Schedule.php
@@ -351,7 +351,13 @@ class Aoe_Scheduler_Model_Schedule extends Mage_Cron_Model_Schedule {
      * @return bool
      */
     public function isAlwaysTask() {
-        return $this->getJobConfiguration()->isAlwaysTask();
+        $isAlwaysTask = false;
+        try {
+            $isAlwaysTask = $this->getJobConfiguration()->isAlwaysTask();
+        } catch (Exception $e) {
+            Mage::logException($e);
+        }
+        return $isAlwaysTask;
     }
 
     /**
