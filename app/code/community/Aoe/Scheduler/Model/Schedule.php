b4e815f5f9aa09a50ab1d3fd498be6a4e377fef2
Fabrizio Branca <fabrizio.branca@aoe.com>
2014-07-17 21:34:16 -0700

    Added event prefix (thanks, davidverholen)

--- app/code/community/Aoe/Scheduler/Model/Schedule.php
+++ app/code/community/Aoe/Scheduler/Model/Schedule.php
@@ -35,6 +35,13 @@ class Aoe_Scheduler_Model_Schedule extends Mage_Cron_Model_Schedule {
     CONST STATUS_DISAPPEARED = 'gone'; // the status field is limited to 7 characters
     CONST STATUS_DIDNTDOANYTHING = 'nothing';
 
+    /**
+    * Prefix of model events names
+    *
+    * @var string
+    */
+    protected $_eventPrefix = 'aoe_scheduler_schedule';
+
 	/**
 	 * @var Aoe_Scheduler_Model_Configuration
 	 */
